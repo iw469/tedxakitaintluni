0df4599c14f0edfab657eff8834c7bfac096740e
QsmileQ <ivanwong0469@gmail.com>
2026-03-18 13:16:29 +0900

    error fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,16 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://iw469.github.io",
-  base: "/tedxakitaintluni",
+  site: 'https://iw469.github.io',
+  base: '/tedxakitaintluni',
   integrations: [mdx(), sitemap(), icon()],
   vite: {
     plugins: [tailwindcss()],
-  },
+    build: {
+      rollupOptions: {
+        external: ['virtual:astro-icon']   // ← this is the key line
+      }
+    }
+  }
 });

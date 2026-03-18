33af11d0e9e51232db66d9311137c717f995440b
QsmileQ <ivanwong0469@gmail.com>
2026-03-18 13:14:05 +0900

    /

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   site: "https://iw469.github.io",
-  base: "tedxakitaintluni",
+  base: "/tedxakitaintluni",
   integrations: [mdx(), sitemap(), icon()],
   vite: {
     plugins: [tailwindcss()],

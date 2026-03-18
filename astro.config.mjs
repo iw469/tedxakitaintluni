5b3e6952c22c0bfe25363daf9463e7affb6bb672
QsmileQ <ivanwong0469@gmail.com>
2026-03-18 13:32:51 +0900

    retest

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,12 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://iw469.github.io',
-  base: '/tedxakitaintluni',
+  base: '/tedxakitaintluni' ,
   integrations: [mdx(), sitemap(), icon()],
   vite: {
     plugins: [tailwindcss()],
-    build: {
-      rollupOptions: {
-        external: ['virtual:astro-icon']   // ← this is the key line
-      }
-    }
-  }
+  },
 });

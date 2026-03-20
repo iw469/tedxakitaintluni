2ceca3a6e6dc32dde3a2a73d4ffb96284ae52d3c
QsmileQ <ivanwong0469@gmail.com>
2026-03-20 19:14:29 +0900

    config changed back

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,12 @@ import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
-import { fileURLToPath } from "url";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://iw469.github.io',
-  base: '/tedxakitaintluni' ,
+  site: "https://astroship.web3templates.com",
   integrations: [mdx(), sitemap(), icon()],
   vite: {
     plugins: [tailwindcss()],
-    resolve: {
-      alias: {
-        // allow using imports like "@/assets/..." to reference src/
-        "@": fileURLToPath(new URL("./src", import.meta.url)),
-      },
-    },
   },
 });

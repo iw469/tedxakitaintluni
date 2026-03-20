5fcb3226bf5a7d8a6284613b6fecb7530d5a243e
QsmileQ <ivanwong0469@gmail.com>
2026-03-20 19:01:54 +0900

    error fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
+import { fileURLToPath } from "url";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +12,11 @@ export default defineConfig({
   integrations: [mdx(), sitemap(), icon()],
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        // allow using imports like "@/assets/..." to reference src/
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
   },
 });
